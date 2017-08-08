9ec4f1b625cb55c31798420f27161f869f0d4f46
seochangwook <scw05313315@gmail.com>
2017-08-08 10:51:44 +0900

    fix - 1 (common - nav)

--- forweaver/src/main/java/com/forweaver/util/GitUtil.java
+++ forweaver/src/main/java/com/forweaver/util/GitUtil.java
@@ -67,7 +67,7 @@ public class GitUtil implements VCUtil{
 		this.gitPath = gitPath;
 	}
 	public GitUtil(){
-		this.gitPath = "/home/git/"; //로컬 깃 저장소 주소
+		this.gitPath = "/Users/macbook/project/git/"; //로컬 깃 저장소 주소
 	}
 
 	/** 프로젝트 초기화 메서드
